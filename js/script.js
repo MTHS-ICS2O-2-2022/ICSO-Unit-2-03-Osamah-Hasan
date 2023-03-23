91ada45e696bd6b2cd35900ccec2be478db0c22f
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-03-23 17:13:00 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Osamah All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Osamah
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("first-name").value
+  const userhousenumber = parseInt(document.getElementById("House").value)
+  const userStreetname = document.getElementById("Street").value
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your info is: " + firstName + userhousenumber + userStreetname + "."
+}
